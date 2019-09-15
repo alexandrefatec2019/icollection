9c97c7b0bdaa24dc2fa6a990aeffcba321419720
alexandrefatec2019 <47427719+alexandrefatec2019@users.noreply.github.com>
2019-09-15 12:04:14 -0300

    Oie2

---
+++ lib/AppBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Color backgroundColor = Colors.black38;
+  final AppBar appBar;
+  final List<Widget> widgets;
+
+  /// you can add more fields that meet your needs
+
+  const BaseAppBar({Key key, this.appBar, this.widgets}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        'iCollection',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: backgroundColor,
+      actions: widgets,
+    );
+  }
+
+  @override
+  Size get preferredSize => new Size.fromHeight(appBar.preferredSize.height);
+}
