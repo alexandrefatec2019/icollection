dae089a0ed2d23adeb7ee05c1c127c3e098f8cdb
alexandrefatec2019 <47427719+alexandrefatec2019@users.noreply.github.com>
2019-11-10 17:20:52 -0300

    Tela principal ficando show de bola

--- lib/Produto/ListarProdutosPrincipal.dart
+++ lib/Produto/ListarProdutosPrincipal.dart
@@ -8,6 +8,7 @@ import 'package:icollection/Produto/Produto_Services.dart';
 import 'package:icollection/Produto/note_screen.dart';
 import 'package:icollection/model/listaprodutoModel.dart';
 
+import 'package:show_dialog/show_dialog.dart' as dialog;
 //Futuramente será formatado, mas ja traz a lista dos produtos do firabase e a rolagem funciona
 
 class ListarProdutosPrincipal extends StatefulWidget {
@@ -53,37 +54,62 @@ class _ListarProdutosPrincipalState extends State<ListarProdutosPrincipal> {
         child: ListView.builder(
             //NetworkImage('${items[position].image[0]}')),
             itemCount: items.length,
-            padding: const EdgeInsets.all(0.0),
+            //padding: const EdgeInsets.all(0.0),
             itemBuilder: (context, position) {
               return Column(children: <Widget>[
-                Container(
-                  width: 30.0,
-                  height: 30.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: CachedNetworkImageProvider(
-                          '${items[position].imageUsuario}'),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+                  child: SizedBox(
+                    //tamanho da faixa branca onde vai avatar usuario e nome e mais um botao talvez
+                    height: 60,
+                    child: Row(
+                      children: <Widget>[
+                        //Avatar do usuario
+                        Container(
+                          width: 45.0,
+                          height: 45.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: CachedNetworkImageProvider(
+                                  '${items[position].imageUsuario}'),
+                            ),
+                          ),
+                        ),
+                        //Nome do usuario
+                        Container(
+                            child: Padding(
+                          padding: EdgeInsets.only(left: 5),
+                          child: Text('${items[position].nomeUsuario}',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                  color: Colors.black)),
+                        )),
+                        //Icon
+                        Expanded(
+                            child: Container(
+                                alignment: Alignment.centerRight,
+                                child: Padding(
+                                    padding: EdgeInsets.only(right: 5),
+                                    child: IconButton(
+                                        icon: Icon(Icons.more_vert),
+                                        iconSize: 30,
+                                        tooltip: 'Increase volume by 10',
+                                        onPressed: () {
+                                          dialog.aboutDialog(
+                                              context,
+                                              'oie ' +
+                                                  '${items[position].nomeUsuario}',
+                                              'xxxx');
+                                        }))))
+                      ],
                     ),
                   ),
                 ),
-                SizedBox(
-                    width: 230,
-                    height: 50,
-                    child: //Nome do usuario e avatar = do instagram
-                        Container(
-                            child: Padding(
-                      padding: EdgeInsets.all(1),
-                      child: Text('${items[position].nomeUsuario}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.black)),
-                    ))),
+                //Imagem do produto
                 Container(
-
                     //TODO ver depois para deixar o tamanho maximo
-
                     height: MediaQuery.of(context).size.height / 2,
                     child: AspectRatio(
                         aspectRatio: 500 / 500,
@@ -97,10 +123,30 @@ class _ListarProdutosPrincipalState extends State<ListarProdutosPrincipal> {
                                     image: NetworkImage(
                                         '${items[position].image[0]}')),
                               ),
-                            )
+                            ),
+                            
                           ],
-                        )))
-              ]);
+                        ))),
+                        SizedBox(
+                              height: 80,
+                              //width: 300,
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                      child: Padding(
+                                    padding: EdgeInsets.only(left: 5),
+                                    child: Text(
+                                        '${items[position].nomeproduto}',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 14,
+                                            color: Colors.black)),
+                                  )),
+                                ],
+                              ),
+                            )
+              ]
+              );
             }));
   }
 }
